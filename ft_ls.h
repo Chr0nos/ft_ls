43487301a14ee30a09497ad110522f48ccd78b10
Chr0nos <snicolet@student.42.fr>
2016-01-08 00:41:52 +0100

    ajout de la gestion de plusieurs dossiers

--- ft_ls.h
+++ ft_ls.h
@@ -6,25 +6,26 @@
 /*   By: snicolet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/07 22:52:56 by snicolet          #+#    #+#             */
-/*   Updated: 2016/01/07 23:05:42 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/01/07 23:47:38 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_LS_H
 # define FT_LS_H
 
+#include <dirent.h>
 #include <string.h>
 #include <time.h>
 
 typedef struct	s_file
 {
-	char		*name;
-	size_t		namelen;
-	char		*path;
-	size_t		size;
-	time_t		ctime;
-	time_t		mtime;
-	time_t		atime;
-}				t_file;
+	struct dirent	*de;
+	char			*name;
+	char			*path;
+	size_t			size;
+	time_t			ctime;
+	time_t			mtime;
+	time_t			atime;
+}					t_file;
 
 #endif
--- main.c
+++ main.c
@@ -6,15 +6,17 @@
 /*   By: snicolet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/07 22:56:08 by snicolet          #+#    #+#             */
-/*   Updated: 2016/01/07 23:09:16 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/01/08 00:12:17 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "ft_ls.h"
 #include <dirent.h>
+#include <string.h>
+#include <stdlib.h>
 
-int		ls_dir(char *dir)
+int			ls_dir(char *dir, t_list **lst, int rec)
 {
 	t_file			file;
 	DIR				*d;
@@ -24,19 +26,52 @@ int		ls_dir(char *dir)
 		return (0);
 	while ((ent = readdir(d)))
 	{
-		file.namelen = ent->d_namlen;
-		file.name = ent->d_name;
-		ft_putendl(file.name);
+		file.path = ft_strdup(dir);
+		file.de = ft_memdup(ent, sizeof(struct dirent));
+		file.name = file.de->d_name;
+		ft_lstpush_back(lst, ft_lstnew(&file, sizeof(t_file)));
+		(void)rec;
 	}
+	closedir(d);
 	return (1);
 }
 
-int		main(int ac, char **av)
+static void	ft_lstatomisator(void *x, size_t size)
 {
-	if (ac == 1)
+	t_file	*f;
+
+	(void)size;
+	f = (t_file*)x;
+	free(f->de);
+	free(x);
+}
+
+void		display(t_list *lst)
+{
+	t_file	*file;
+
+	while (lst)
 	{
-		ls_dir(".");
+		file = (t_file*)(lst->content);
+		ft_putendl(file->de->d_name);
+		lst = lst->next;
 	}
-	(void)av;
+}
+
+int			main(int ac, char **av)
+{
+	t_list	*lst;
+
+	lst = NULL;
+	if (ac == 1)
+		ls_dir(".", &lst, 0);
+	else
+		while (--ac)
+			ls_dir(av[ac], &lst, 0);
+	if (lst)
+	{
+		display(lst);
+		ft_lstdel(&lst, &ft_lstatomisator);
+	}
 	return (0);
 }
