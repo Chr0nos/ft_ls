063decba715c9acb42ce5d557196a4b334e22375
Chr0nos <snicolet@student.42.fr>
2016-01-11 23:28:34 +0100

    fix du dossier affiche

--- display.c
+++ display.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/08 16:38:47 by snicolet          #+#    #+#             */
-/*   Updated: 2016/01/10 19:39:42 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/01/11 23:26:49 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -82,7 +82,7 @@ void		display(t_list *lst)
 	{
 		dir = (t_dir*)lst->content;
 		if (dirs > 1)
-			ft_printf("%s:\n", dir->path);
+			ft_printf("%s:\n", dir->pathinfo.path);
 		if (dir->flags & LONG)
 			ft_printf("Total: %d\n", (int)dir->size);
 		dl = dir->content;
