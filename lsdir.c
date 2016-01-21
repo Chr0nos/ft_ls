7f4589df27af66f13072b5dffd12ec8ad3b1991a
Chr0nos <snicolet@student.42.fr>
2016-01-21 13:52:44 +0100

    avancement de la fonctionalite pour ls des fichiers

--- lsdir.c
+++ lsdir.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/08 16:40:26 by snicolet          #+#    #+#             */
-/*   Updated: 2016/01/21 13:12:10 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/01/21 13:48:43 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,7 @@ static char			*getpath(char *dir, char *file)
 	fp = ft_strunsplit(tab, '/');
 	return (fp);
 }
+
 /*
 ** this function add ONE FILE to a dir file list
 ** return: a pointer to the t_file if success, NULL otherwise
@@ -44,7 +45,7 @@ static t_file		*ls_addfile(t_dir *rdir, const char *name, int (*sort)())
 	file->fullpath = getpath(rdir->pathinfo.path, file->name);
 	if (stat(file->fullpath, &file->stats) < 0)
 	{
-		ft_printf("failed to stat: %s\n", name);
+		ft_printf("failed to stat: %s\n", file->fullpath);
 		free(file->fullpath);
 		free(file->name);
 		free(file);
@@ -56,21 +57,31 @@ static t_file		*ls_addfile(t_dir *rdir, const char *name, int (*sort)())
 	if (rdir->flags & NOSORT)
 		ft_lstpush_back(&rdir->content, ft_lstnewlink(file, sizeof(t_file)));
 	else
-		ft_lstpush_sort(&rdir->content, ft_lstnewlink(file, sizeof(t_file)), 
+		ft_lstpush_sort(&rdir->content, ft_lstnewlink(file, sizeof(t_file)),
 				sort);
 	return (file);
 }
 
-static DIR			*ls_dir_open(char *dir)
+static DIR			*ls_dir_open(t_dir *rdir)
 {
 	DIR					*d;
+	struct	stat		stats;
+	const char			*dir = rdir->pathinfo.path;
 
 	if (!(d = opendir(dir)))
 	{
-		ft_putstr_fd("ft_ls: ", 2);
-		ft_putstr_fd(dir, 2);
-		ft_putstr_fd(": Failed to open\n", 2);
-		return (NULL);
+		if (stat(dir, &stats) >= 0)
+		{
+			ft_strcpy(rdir->pathinfo.path, ".");
+			ls_addfile(rdir, dir, (int(*)())getsorter(rdir->flags));
+		}
+		else
+		{
+			ft_putstr_fd("ft_ls: ", 2);
+			ft_putstr_fd(dir, 2);
+			ft_putstr_fd(": Failed to open\n", 2);
+			return (NULL);
+		}
 	}
 	return (d);
 }
@@ -88,7 +99,7 @@ void				ls_dir(t_list **root, t_dir *rdir)
 	t_file			*file;
 	int				(*sort)();
 
-	if (!(d = ls_dir_open(rdir->pathinfo.path)))
+	if (!(d = ls_dir_open(rdir)))
 		return ;
 	sort = (int(*)())getsorter(rdir->flags);
 	while ((ent = readdir(d)))
