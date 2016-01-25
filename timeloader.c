cad643e73182376e40460aceefdacc908162f25e
Chr0nos <snicolet@student.42.fr>
2016-01-25 18:00:22 +0100

    formatage plus proche du vrais ls

--- timeloader.c
+++ timeloader.c
@@ -3,23 +3,13 @@
 
 void	timeloader(const char *time, char *buffer)
 {
-	int		space;
-	int		len;
 	int		timelen;
 
-	space = 0;
-	while ((*time) && (*time != ' ') && (space++ < 2))
-		time++;
-	time += 2;
 	timelen = (int)ft_strlen(time);
-	ft_memcpy(buffer, time + 4, 3);
-	ft_memcpy(buffer + 3, time, 4);
-	ft_strcpy(buffer + 7, time + 7);
-	len = (int)ft_strlen(buffer);
-	if (ft_strcmp(buffer + len - 5, "2016\n") == 0)
-		buffer[len - 8] = '\0';
-	else
+	ft_strncpy(buffer, time + 4, (size_t)(timelen - 12));
+	if (ft_strcmp(time + timelen - 5, "2016\n") != 0)
 	{
-		//todo: gestion de la suppression des secondes sans l annee
+		ft_strcat(buffer, " ");
+		ft_strncat(buffer, time + timelen - 5, 5);
 	}
 }
