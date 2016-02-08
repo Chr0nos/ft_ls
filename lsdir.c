99b8ae41fc84ba300ea3c7e9a6d1f4ce678c329f
Sébastien Nicolet <snicolet@student.42.fr>
2016-02-08 02:00:46 +0100

    fix d un message d erreur en ft_ls vers ls

--- lsdir.c
+++ lsdir.c
@@ -111,7 +111,7 @@ static DIR			*ls_dir_open(t_dir *rdir)
 		}
 		else
 		{
-			ft_strcpy(b, "ft_ls: ");
+			ft_strcpy(b, "ls: ");
 			ft_strcat(b, dir);
 			ft_strcat(b, ": ");
 			ft_strcat(b, strerror(errno));
