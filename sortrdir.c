f13fe3b2026095f0a071e5313dfedae63031d0cb
Sebastien N <snicolet@student.42.fr>
2016-05-01 18:56:25 +0200

    gestion du -Rr

--- sortrdir.c
+++ sortrdir.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <snicolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/01 17:19:10 by snicolet          #+#    #+#             */
-/*   Updated: 2016/05/01 17:19:42 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/05/01 18:54:49 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,8 @@
 
 int			ls_sortrdir(t_list *a, t_list *b)
 {
+	if (((t_dir*)a->content)->flags & REVERSESORT)
+		return (-ft_strcmp(((t_dir*)a->content)->path,
+			((t_dir*)b->content)->path));
 	return (ft_strcmp(((t_dir*)a->content)->path, ((t_dir*)b->content)->path));
 }
